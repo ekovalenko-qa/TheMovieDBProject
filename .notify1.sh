07dc1baf363cc71ae39cd490a14fbb9ad5f01aa5
ekovalenko <katia.kovalenko@mail.ru>
2021-02-21 09:23:25 +0600

    fix reporting for API

--- .notify1.sh
+++
@@ -1,3 +0,0 @@
-TEXT="Тестирование выполнено: (☉_☉) %0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0A
-отчет доступен по ссылке:"
-curl -s -X POST https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_USER_ID -d text="$TEXT"
